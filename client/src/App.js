7934764d1841ea07175f53f320bc46300b358d93
Kennan Smith <kennan.smith1@gmail.com>
2018-07-03 17:43:01 -0600

    added home page and replaced current simple navbar with the navbar I used in the original myData project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-class App extends Component {
-  render() {
-    return (
-      <div>Hello World!</div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
 
 export default App;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a className="navbar-brand" href="/">
-      React Reading List
-    </a>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">myData</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+  </button>
+  
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+      <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pages
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+
+          {/* <a className="dropdown-item" href="/calorie">Calorie Counter</a> */}
+          
+          <a className="dropdown-item" href="/spending">Spending Tracker</a>
+          </div>
+      </li>
+      </ul>
+  </div>
   </nav>
 );
 
