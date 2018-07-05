2352222335681025866e5dc0d7e6d8835f1c0912
Kennan Smith <kennan.smith1@gmail.com>
2018-07-05 16:28:48 -0600

    added user axios calls to client/src/utils/API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,25 @@
 import axios from "axios";
 
 export default {
+  // USER SECTION
+  // Gets all Users 
+  getUser: function() {
+    return axios.get("/api/user");
+  },
+  // Gets the user with the given id
+  getUser: function(id) {
+    return axios.get("/api/user/" + id);
+  },
+  // Deletes the user with the given id
+  deleteUser: function(id) {
+    return axios.delete("/api/user/" + id);
+  },
+  // Saves a user to the database
+  saveUser: function(spendingData) {
+    return axios.post("/api/user", spendingData);
+  },
+
+  // SPENDING SECTION
   // Gets all Spending entries
   getSpending: function() {
     return axios.get("/api/spending");
