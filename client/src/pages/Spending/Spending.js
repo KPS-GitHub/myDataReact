68655250dd6ca830af55c501170ead4541873579
Kennan Smith <kennan.smith1@gmail.com>
2018-07-09 20:51:41 -0600

    further tweaking on line chart - mouseover code did not compile so it's commented out for now; also slightly rearranged the spending page so that it is one big row, the list+form are together in a the leftmost column, the graph(s)+summary stats are together in the rightmost column

--- client/src/pages/Spending/Spending.js
+++ client/src/pages/Spending/Spending.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
-import { LineChart, PieChart, Legend } from "react-easy-chart";
+import { LineChart, PieChart, Legend, ToolTip } from "react-easy-chart";
 import moment from "moment";
 
 class SpendingPage extends Component {
@@ -16,7 +16,13 @@ class SpendingPage extends Component {
         amount: "",
         category: "",
         date: "",
-        dataArr: []
+        dataArr: [],
+        // graph-related state vars
+        showToolTip: false,
+        top: "",
+        left: "",
+        y: 0,
+        x: 0
     };
 
     componentDidMount() {
@@ -121,6 +127,39 @@ class SpendingPage extends Component {
     };
 
     
+    // mouseover code for line chart (doesn't compile yet)
+    // mouseOverHandler(d, e) {
+    //     this.setState({
+    //       showToolTip: true,
+    //       top: `${e.screenY - 10}px`,
+    //       left: `${e.screenX + 10}px`,
+    //       y: d.y,
+    //       x: d.x});
+    //   }
+    
+    //   mouseMoveHandler(e) {
+    //     if (this.state.showToolTip) {
+    //       this.setState({top: `${e.y - 10}px`, left: `${e.x + 10}px`});
+    //     }
+    //   }
+    
+    //   mouseOutHandler() {
+    //     this.setState({showToolTip: false});
+    //   }
+    
+    //   createTooltip() {
+    //     if (this.state.showToolTip) {
+    //       return (
+    //         <ToolTip
+    //           top={this.state.top}
+    //           left={this.state.left}
+    //         >
+    //             The x value is {this.state.x} and the y value is {this.state.y}
+    //         </ToolTip>
+    //       );
+    //     }
+    //     return false;
+    //   }
 
     
 
@@ -146,52 +185,52 @@ class SpendingPage extends Component {
                         ) : (
                             <h3>No Results to Display</h3>
                     )}
+                    {/* add a purchase form */}
+                    <form>
+                        <Input
+                            value={this.state.amount}
+                            onChange={this.handleInputChange}
+                            name="amount"
+                            placeholder="Purchase Amount (required)"
+                        />
+                        <Input
+                            value={this.state.category}
+                            onChange={this.handleInputChange}
+                            name="category"
+                            placeholder="Category (required)"
+                        />
+                        <FormBtn
+                            disabled={!(this.state.amount && this.state.category)}
+                            onClick={this.handleFormSubmit}
+                        >
+                            Submit Purchase
+                        </FormBtn>
+                    </form>
                     </Col>
-                    {/* graphs/data vis */}
+                    {/* graphs/data vis and summary data */}
                     <Col size="lg-8">
                         <Col size="lg-12">
-                        <LineChart
-                            xType={'time'}
-                            axes
-                            margin={{top: 50, right: 50, bottom: 50, left: 50}}
-                            axisLabels={{x: 'Date', y: 'Amount ($)'}}
-                            width={600}
-                            interpolate={'linear'}
-                            height={400}
-                            data={this.state.dataArr}
-                            style={{ '.label': { fill: 'black' } }}
-                        />
+                            <LineChart
+                                xType={'time'}
+                                axes
+                                dataPoints
+                                margin={{top: 50, right: 50, bottom: 50, left: 50}}
+                                axisLabels={{x: 'Date', y: 'Amount ($)'}}
+                                // mouseOverHandler={this.mouseOverHandler}
+                                // mouseOutHandler={this.mouseOutHandler}
+                                // mouseMoveHandler={this.mouseMoveHandler}
+                                width={600}
+                                interpolate={'linear'}
+                                height={400}
+                                data={this.state.dataArr}
+                                style={{ '.label': { fill: 'black' } }}
+                            />
+                        </Col>
+                        
+                        <Col size="lg-12">
+                            {/* Summary Stats */}
                         </Col>
                     </Col>
-                </Row>
-                <Row>
-                    {/* purchase form */}
-                    <Col size="lg-4">
-                        <form>
-                            <Input
-                                value={this.state.amount}
-                                onChange={this.handleInputChange}
-                                name="amount"
-                                placeholder="Purchase Amount (required)"
-                            />
-                            <Input
-                                value={this.state.category}
-                                onChange={this.handleInputChange}
-                                name="category"
-                                placeholder="Category (required)"
-                            />
-                            <FormBtn
-                                disabled={!(this.state.amount && this.state.category)}
-                                onClick={this.handleFormSubmit}
-                            >
-                                Submit Purchase
-                            </FormBtn>
-                        </form>
-                    </Col>
-                    {/* Summary Stats */}
-                    <Col size="lg-8">
-                    
-                    </Col>                   
                 </Row>
             </Container>
         );
